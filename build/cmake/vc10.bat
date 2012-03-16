39033f88d531c39c67261effe80276a3836a9db6
stefan <>
2012-03-16 08:09:09 +0000

    added support for vc10 and added ignores for cmake build directories

---
+++ build/cmake/vc10.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set PWD=%~dp0
+set SRC_ROOT=%PWD%\..\..\
+set BUILD_ROOT=%PWD%\build-vc10
+
+mkdir %BUILD_ROOT%
+cd %BUILD_ROOT%
+
+cmake -G "Visual Studio 10" %SRC_ROOT%
+
+
+cd %PWD%
