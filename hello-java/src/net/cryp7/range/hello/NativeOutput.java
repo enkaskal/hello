6127a48f86beec18d8a9efd389e6b240a0c3702c
stefan <>
2012-09-02 10:12:11 +0000

    confirmed that previous change fixes linux build for release, but i missed debug. now fixed :)

--- hello-java/src/net/cryp7/range/hello/NativeOutput.java
+++ hello-java/src/net/cryp7/range/hello/NativeOutput.java
@@ -40,34 +40,42 @@ public class NativeOutput
 				{
 					try
 					{
-						System.load(pwd.getAbsolutePath() + "/../lib/liboutput.so");
-						System.load(pwd.getAbsolutePath() + "/../lib/liboutput-jni.so");
+						System.load(pwd.getAbsoluteFile() + "/liboutput_d.so");
+						System.load(pwd.getAbsoluteFile() + "/liboutput-jni_d.so");
 					}
 					catch (UnsatisfiedLinkError exce)
 					{
 						try
 						{
-							System.load(pwd.getAbsolutePath() + "/../lib/liboutput_d.so");
-							System.load(pwd.getAbsolutePath() + "/../lib/liboutput-jni_d.so");
+							System.load(pwd.getAbsolutePath() + "/../lib/liboutput.so");
+							System.load(pwd.getAbsolutePath() + "/../lib/liboutput-jni.so");
 						}
 						catch (UnsatisfiedLinkError excep)
 						{
 							try
 							{
-								System.load(pwd.getAbsolutePath() + "/../build/cmake/build-vc8/artifacts/debug/liboutput_d.so");
-								System.load(pwd.getAbsolutePath() + "/../build/cmake/build-vc8/artifacts/debug/liboutput-jni_d.so");
+								System.load(pwd.getAbsolutePath() + "/../lib/liboutput_d.so");
+								System.load(pwd.getAbsolutePath() + "/../lib/liboutput-jni_d.so");
 							}
 							catch (UnsatisfiedLinkError except)
 							{
 								try
 								{
-									System.load(pwd.getAbsolutePath() + "/build-ci/artifacts/release/liboutput.so");
-									System.load(pwd.getAbsolutePath() + "/build-ci/artifacts/release/liboutput-jni.so");
+									System.load(pwd.getAbsolutePath() + "/../build/cmake/build-vc8/artifacts/debug/liboutput_d.so");
+									System.load(pwd.getAbsolutePath() + "/../build/cmake/build-vc8/artifacts/debug/liboutput-jni_d.so");
 								}
 								catch (UnsatisfiedLinkError excepti)
 								{
-									System.load(pwd.getAbsolutePath() + "/build-ci/artifacts/liboutput.so");
-									System.load(pwd.getAbsolutePath() + "/build-ci/artifacts/liboutput-jni.so");
+									try
+									{
+										System.load(pwd.getAbsolutePath() + "/build-ci/artifacts/release/liboutput.so");
+										System.load(pwd.getAbsolutePath() + "/build-ci/artifacts/release/liboutput-jni.so");
+									}
+									catch (UnsatisfiedLinkError exceptio)
+									{
+										System.load(pwd.getAbsolutePath() + "/build-ci/artifacts/liboutput.so");
+										System.load(pwd.getAbsolutePath() + "/build-ci/artifacts/liboutput-jni.so");
+									}
 								}
 							}
 						}
